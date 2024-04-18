4379a0c6a60448ea5d3df595677ae2384ac4a782
Muhammad Fargazzi <mfargazzy@gmail.com>
2024-04-18 22:22:07 +0700

    final

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}',
+  './**/*.{html,js}',],
+  theme: {
+    extend: {
+      fontFamily: {
+        chakra: ['"Chakra Petch"', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
